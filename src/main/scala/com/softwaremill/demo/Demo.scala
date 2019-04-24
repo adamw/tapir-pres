7ba2c8ab56cfdcbe4ab37f876e1c4ee96c322ab5
adamw <adam@warski.org>
2019-04-24 12:06:22 +0200

    Base demo class

---
+++ src/main/scala/com/softwaremill/demo/Demo.scala
@@ -0,0 +1,13 @@
+package com.softwaremill.demo
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object Demo extends App {
+
+}
